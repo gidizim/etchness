288a08aea09091db464071370fe12a9ff9311d42
Sarah <60539445+Sarah9768@users.noreply.github.com>
2021-03-05 17:15:29 +1100

    Show + edit profile

--- login.js
+++ login.js
@@ -1,3 +1,15 @@
 const login = () => {
     location.href = "home.html";
 }
+
+const edit = () => {
+    const profileFeatures = document.getElementsByName('profile');
+    if (document.getElementById('edit-btn').value == "Save changes") {
+        document.getElementById('edit-btn').value = "Edit profile";
+    } else {
+        document.getElementById('edit-btn').value = "Save changes";
+    }
+    for (let i = 0; i < profileFeatures.length; i++) {
+        profileFeatures[i].disabled = !profileFeatures[i].disabled;
+    }
+}
