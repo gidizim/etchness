1a936da3d4e0aa7f2e054a63b6b20383903841f3
Sarah <60539445+Sarah9768@users.noreply.github.com>
2021-03-06 20:58:35 +1100

    Changed style of pages

--- login.js
+++ login.js
@@ -13,10 +13,13 @@ const login = () => {
 
 const edit = () => {
     const profileFeatures = document.getElementsByName('profile');
-    if (document.getElementById('edit-btn').value == "Save changes") {
-        document.getElementById('edit-btn').value = "Edit profile";
+    const button = document.getElementById('edit-btn');
+    if (button.value == "Save changes") {
+        button.value = "Edit profile";
+        button.style.backgroundColor = "#332e2e";
     } else {
-        document.getElementById('edit-btn').value = "Save changes";
+        button.value = "Save changes";
+        button.style.backgroundColor = "rgb(128, 39, 39)";
     }
     for (let i = 0; i < profileFeatures.length; i++) {
         profileFeatures[i].disabled = !profileFeatures[i].disabled;
