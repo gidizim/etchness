a7d919230ef7883b5975480ab1f242f8951b6bfe
Tony Liao <z5259804@ad.unsw.edu.au>
2021-03-11 21:01:58 +1100

    shadow added

--- deliv1/deliv1HIFI/js/navbarload.js
+++ deliv1/deliv1HIFI/js/navbarload.js
@@ -27,23 +27,3 @@ $(function(){
 
 
 $("#nav")
-
-/* changes nav bar depending on active page*/
-//$(document).ready(function() {
-    //alert("The current page path is " + window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1));
-
-
-//    var path = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
-//    if (path == 'home.html')
-//        $('#login').addClass('active');
-//    else if (path == 'login.html')
-//        $('#login').addClass('active');
-//    else if (path == 'watchlist.html')
-//        $('#watchlist').addClass('active');
-//    else if (path == 'profile.html')
-//        $('#profile').addClass('active');
-//    else if (path == 'newsfeed.html')
-//        $('#newsfeed').addClass('active');
-//
-//});
-
