4144eeba03b49f0780f1acaa8ee53a2d9dc44627
Michael Chen <chaosspaces11@gmail.com>
2021-03-11 20:52:06 +1100

    Fixed active pages

--- deliv1/deliv1HIFI/js/navbarload.js
+++ deliv1/deliv1HIFI/js/navbarload.js
@@ -1,6 +1,23 @@
+/* Function to run once navbar has loaded */
+function navLoaded()
+{
+    var path = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
+    if (path == 'home.html')
+        $('#home').addClass('active');
+    else if (path == 'login.html')
+        $('#login').addClass('active');
+    else if (path == 'watchlist.html')
+        $('#watchlist').addClass('active');
+    else if (path == 'profile.html')
+        $('#profile').addClass('active');
+    else if (path == 'newsfeed.html')
+        $('#newsfeed').addClass('active');
+}
+
+
 /* loads the header component*/
 $(function(){
-    $("#nav").load("components/navbar.html");
+    $("#nav").load("components/navbar.html", navLoaded);
 });
 
 /* loads the footer component*/
@@ -8,22 +25,25 @@ $(function(){
     $("#footer").load("components/footer.html");
 });
 
-/* changes nav bar depending on active page*/
-$(document).ready(function() {
-    //alert("The current page path is " + window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1));
- 
-    var path = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
- 
-    if(path == 'home.html')
-        $('#home').addClass('active');
-    else if(path == 'login.html')
-        $('#login').addClass('active');
-    else if(path == 'watchlist.html')
-        $('#watchlist').addClass('active');
-    else if(path == 'profile.html')
-        $('#profile').addClass('active');
-    else if(path == 'newsfeed.html')
-        $('#newsfeed').addClass('active');
 
-});
+$("#nav")
+
+/* changes nav bar depending on active page*/
+//$(document).ready(function() {
+    //alert("The current page path is " + window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1));
+
+
+//    var path = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
+//    if (path == 'home.html')
+//        $('#login').addClass('active');
+//    else if (path == 'login.html')
+//        $('#login').addClass('active');
+//    else if (path == 'watchlist.html')
+//        $('#watchlist').addClass('active');
+//    else if (path == 'profile.html')
+//        $('#profile').addClass('active');
+//    else if (path == 'newsfeed.html')
+//        $('#newsfeed').addClass('active');
+//
+//});
 
