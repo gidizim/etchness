bafb7af7c932d0cf7d665272358dfc77ea766a45
Tony Liao <z5259804@ad.unsw.edu.au>
2021-04-03 14:40:36 +1100

    gitignore, requirements and newsapi added

--- server/server.py
+++ server/server.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from flask import render_template, url_for
+
+from newsapi import NewsApiClient
+newsapi = NewsApiClient(api_key='db0f90a0065445e786863c1ce792fef6')
+
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          category='business',
+                                          language='en',
+                                          country='us')
+
 app = Flask(__name__)
 
 @app.route('/')
