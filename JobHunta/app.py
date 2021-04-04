fd43ea8ecaeda31a946ee125eb3560d6263e2e24
Sarah <60539445+Sarah9768@users.noreply.github.com>
2021-04-05 00:50:47 +1000

    Moved getJobs + added init file

--- JobHunta/app.py
+++ JobHunta/app.py
@@ -1,5 +1,6 @@
 from flask.templating import render_template_string
 from server.newsfeed import getNews
+from server.getJobs import get_combined_results
 from flask import Flask
 from flask import render_template, url_for
 app = Flask(__name__)
@@ -7,10 +8,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
 @app.route('/newsfeed')
 def get_news():
     articles = getNews()
     return render_template('newsfeed.html', articles=articles)
+
+# browser gets data
+@app.route('/search')
+def get_search_results(descrip, location, full_time, part_time, contract, permanent, salary_min, page):
+    jobs = get_combined_results(descrip, location, full_time, part_time, contract, permanent, salary_min, page)
+    return render_template('results.html', jobs=jobs)
+
+# get_combined_results('software', 'Sydney', '', 1, '', '', 100000, 1)
 
 @app.route('/components/<file>')
 def get_component(file="home.html"):
