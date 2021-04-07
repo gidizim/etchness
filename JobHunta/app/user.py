e3cec121a685401273162b5df526522635ab0bfc
Michael Chen <chaosspaces11@gmail.com>
2021-04-07 20:05:30 +1000

    Fixed user functions

--- JobHunta/app/user.py
+++ JobHunta/app/user.py
@@ -14,6 +14,7 @@ def get_user_details(u_id):
 
         raise ValueError("Invalid user id")
 
+    data = data[0]
     result = {}
     result['email'] = data['email']
     result['password'] = '**********'
