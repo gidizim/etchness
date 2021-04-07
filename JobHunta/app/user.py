717414f51c3ef655c0ebacc7ad07a2a3128cacd0
Michael Chen <chaosspaces11@gmail.com>
2021-04-07 19:28:00 +1000

    Added functions to set and get user profile as well as reset password

---
+++ JobHunta/app/user.py
@@ -0,0 +1,69 @@
+from . import db
+
+# Returns a dictionary of user details from the database
+def get_user_details(u_id):
+    conn = db.get_db()
+    cur = conn.cursor()
+
+
+    cur.execute("SELECT * FROM user WHERE id = '%s';" % u_id)
+
+    data = cur.fetchall()
+    if len(data) == 0:
+        db.close_db()
+
+        raise ValueError("Invalid user id")
+
+    result = {}
+    result['email'] = data['email']
+    result['password'] = '**********'
+    result['first_name'] = data['first_name']
+    result['last_name'] = data['last_name']
+
+    db.close_db()
+
+    return result
+
+# Returns a dictionary of user details from the database
+def set_user_details(u_id, email, first_name, last_name):
+    conn = db.get_db()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM user WHERE id = '%s';" % u_id)
+
+    data = cur.fetchall()
+    if len(data) == 0:
+        db.close_db()
+
+        raise ValueError("Invalid user id")
+
+    cur.execute("UPDATE user SET email = '%s', first_name = '%s', last_name = '%s' WHERE id = '%s'"
+                % (email, first_name, last_name, u_id))
+
+    conn.commit()
+    db.close_db()
+
+def reset_password(u_id, hashed_password):
+    conn = db.get_db()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM user WHERE id = '%s';" % u_id)
+
+    data = cur.fetchall()
+    if len(data) == 0:
+        db.close_db()
+
+        raise ValueError("Invalid user id")
+
+    cur.execute("UPDATE user SET password = '%s' WHERE id = '%s'"
+                % (hashed_password, u_id))
+
+    conn.commit()
+    db.close_db()
+
+
+
+
+
+
+
